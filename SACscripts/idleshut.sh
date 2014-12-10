fb74c67befe6b545c3b14199968c99d8f11d30eb
lebrun <tommylebrun@yahoo.com>
2014-12-10 14:34:23 -0500

    Create idleshut.sh

---
+++ SACscripts/idleshut.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Tom LeBrun
+# Mod Hist:
+# Created 121014
+# Modified
+# 
+# The follwoing program shuts down a instance if no one is logged in. 
+
+/bin/netstat -a| grep mikey | grep EST    # Checks for port 2269
+if [ $? -gt 0 ]
+then
+   # No one is logged in, shut'er down
+   /sbin/shutdown -h now
+fi
